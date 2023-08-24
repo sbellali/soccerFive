c5ce2ca8fb64801806a2519a71b81a8c690f409e
Said BELLALI <said.bellali.pro@gmail.com>
2023-08-24 03:14:10 +0200

    creation login form using tailwind

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () => import("./pages/login/login.component").then((module) => module.LoginComponent),
+  },
+  {
+    path: "home",
+    loadComponent: () => import("./pages/home/home.component").then((module) => module.HomeComponent),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  form = this.formBuilder.nonNullable.group({
+    username: ["", [Validators.required, Validators.pattern(/^\w{4,10}$/)]],
+    password: ["", [Validators.required, Validators.minLength(6)]]
+  });
+
+  error= "";
+
+  onSubmit() {
+    console.log("SUBMIT: ", this.form.value)
+  }
+
+  createAccount() {
+    console.log("CREATE: ", this.form.value)
+  }
+}
