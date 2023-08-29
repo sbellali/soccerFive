38603b9eceac8ab32412eaa972cf93138c38422b
Said BELLALI <said.bellali.pro@gmail.com>
2023-08-29 03:30:05 +0200

    Home page structure

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from 'src/app/partials/navbar/navbar.component';
+import { SidebarComponent } from 'src/app/partials/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NavbarComponent, SidebarComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
---
+++ frontend/src/app/partials/user-details/user-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.scss']
+})
+export class UserDetailsComponent {
+
+}
