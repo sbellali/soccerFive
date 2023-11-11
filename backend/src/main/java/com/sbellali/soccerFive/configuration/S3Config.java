cecb1c9b12c5809d29308bde0dbc516fab9bb28c
Said BELLALI <said.bellali.pro@gmail.com>
2023-11-11 20:29:31 +0100

    mock backend with minio

--- backend/src/main/java/com/sbellali/soccerFive/configuration/S3Config.java
+++ backend/src/main/java/com/sbellali/soccerFive/configuration/S3Config.java
@@ -1,5 +1,7 @@
 package com.sbellali.soccerFive.configuration;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +15,15 @@ public class S3Config {
     @Value("${aws.region}")
     private String awsRegion;
 
+    @Value("${aws.s3.uri}")
+    private String awsUri;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(this.awsRegion))
+                .endpointOverride(URI.create(awsUri))
+                .forcePathStyle(true)
                 .build();
 
     }
